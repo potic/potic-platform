7f2361799a6593c494a480e4c6398c621c26f256
Yaroslav Yermilov <yermilov17@gmail.com>
2018-03-08 16:55:24 +0200

    restart mongodb container on failure

--- mongodb/start-mongodb.sh
+++ mongodb/start-mongodb.sh
@@ -17,7 +17,7 @@ warn "Killing currently running docker image..."
 docker kill potic-mongodb; docker rm potic-mongodb
 
 warn "Starting docker image..."
-docker run --name potic-mongodb -v /mnt/data/mongodb:/data/db -d mongo:3.6 --auth
+docker run --name potic-mongodb --restart on-failure -v /mnt/data/mongodb:/data/db -d mongo:3.6 --auth
 
 warn "Currently running docker images"
 docker ps -a
